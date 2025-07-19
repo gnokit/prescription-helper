c4451725e6e92edcbe13fb8cfc3beb32fce59f63
Paul <gnokit@gmail.com>
2025-07-19 16:47:02 +0800

    feat: configure deployment for GitHub Pages
    - Adds gh-pages dependency.
    - Updates package.json with homepage URL and deploy scripts.
    - Configures vite.config.ts with the correct base path for the repository.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/prescription-helper/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
